ce31d3adab7f7241eda081830ef5033dd5f2e215
NabilHG <nabilhajjoune@gmail.com>
2025-05-18 23:40:52 +0200

    fixed pressable EventBlock component, now it can be touched on a device

--- components/home/EventBlock.tsx
+++ components/home/EventBlock.tsx
@@ -1,6 +1,5 @@
 import { Text } from "../ui/text";
 import { Pressable } from "../ui/pressable";
-import { Box } from "../ui/box";
 import { Event } from "@/types/event";
 
 export default function EventBlock({
@@ -14,17 +13,14 @@ export default function EventBlock({
   height: number;
   onPress: () => void;
 }) {
-  const infopadre = onPress;
-  console.log(infopadre);
   return (
-    <Pressable onPress={onPress}>
-      <Box
-        className="absolute bg-blue-500 rounded-lg px-2 py-1"
-        style={{ top, height, left: 15, right: 20 }}
-      >
-        <Text className="text-white text-xs">{event.title}</Text>
-        <Text className="text-white text-[10px]">{`${event.start} - ${event.end}`}</Text>
-      </Box>
+    <Pressable
+      onPress={onPress}
+      className="absolute bg-blue-500 rounded-lg px-2 py-1"
+      style={{ top, height, left: 15, right: 20 }}
+    >
+      <Text className="text-white text-xs">{event.title}</Text>
+      <Text className="text-white text-[10px]">{`${event.start} - ${event.end}`}</Text>
     </Pressable>
   );
 }
